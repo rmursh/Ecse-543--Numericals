8d4ccc9b33462f0825f11f53ff7a5cf5e1e906bc
Razi Murshed <razi.murshed@mail.mcgill.ca>
2016-09-15 19:50:33 -0400

    Removed metadata

--- Assignment-1/CholeskyDecomposition/src/CholeskyDecomposition.java
+++ Assignment-1/CholeskyDecomposition/src/CholeskyDecomposition.java
@@ -8,27 +8,27 @@ public class CholeskyDecomposition {
       double[][] test1 = {{3.75774013057833, 0.699305179151236, 0.212029932955794},
 				{0.699305179151236, 3.17118668781156, 0.491484536490249},
 				{0.212029932955794, 0.491484536490249, 3.04617139063115}};
-		System.out.println(Arrays.deepToString(choleskyDecompose(test1))+ " \n");
-		double[][] test2 = {{18, 22, 54, 42},
-						{22, 70, 86, 62},
-						{54, 86, 174, 134},
-						{42, 62, 134, 106}};
-		System.out.println(Arrays.deepToString(choleskyDecompose(test2))+" \n");
+	  System.out.println(Arrays.deepToString(choleskyDecompose(test1))+ " \n");
+	  double[][] test2 = {{18, 22, 54, 42},
+					{22, 70, 86, 62},
+					{54, 86, 174, 134},
+					{42, 62, 134, 106}};
+	  System.out.println(Arrays.deepToString(choleskyDecompose(test2))+" \n");
 	}
 	
-	public static double[][] choleskyDecompose(double[][] a){
-		int m = a.length;
-		double[][] l = new double[m][m]; //automatically initialzed to 0's
-		for(int i = 0; i< m;i++){
+	public static double[][] choleskyDecompose(double[][] inputMatrix){
+		int lengthOfMatrix = inputMatrix.length;
+		double[][] l_Matrix = new double[lengthOfMatrix][lengthOfMatrix]; //automatically initialzed to 0's
+		for(int i = 0; i< lengthOfMatrix;i++){
 			for(int k = 0; k < (i+1); k++){
 				double sum = 0;
 				for(int j = 0; j < k; j++){
-					sum += l[i][j] * l[k][j];
+					sum += l_Matrix[i][j] * l_Matrix[k][j];
 				}
-				l[i][k] = (i == k) ? Math.sqrt(a[i][i] - sum) : (1.0 / l[k][k] * (a[i][k] - sum));
+				l_Matrix[i][k] = (i == k) ? Math.sqrt(inputMatrix[i][i] - sum) : (1.0 / l_Matrix[k][k] * (inputMatrix[i][k] - sum));
 			}
 		}
-		return l;
+		return l_Matrix;
 	}
 	
 
