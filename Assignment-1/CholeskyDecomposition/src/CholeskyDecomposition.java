07817dbffe8133ef3bd427a3cf78fde4d28f081f
Razi Murshed <razi.murshed@mail.mcgill.ca>
2016-09-18 18:54:56 -0400

    Added code to solve matrix equation using cholesky decomposition tbd

--- Assignment-1/CholeskyDecomposition/src/CholeskyDecomposition.java
+++ Assignment-1/CholeskyDecomposition/src/CholeskyDecomposition.java
@@ -1,35 +1,118 @@
 import java.util.*;
-
-public class CholeskyDecomposition {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-      System.out.println("Hello World \n");
-      double[][] test1 = {{3.75774013057833, 0.699305179151236, 0.212029932955794},
-				{0.699305179151236, 3.17118668781156, 0.491484536490249},
-				{0.212029932955794, 0.491484536490249, 3.04617139063115}};
-	  System.out.println(Arrays.deepToString(choleskyDecompose(test1))+ " \n");
-	  double[][] test2 = {{18, 22, 54, 42},
-					{22, 70, 86, 62},
-					{54, 86, 174, 134},
-					{42, 62, 134, 106}};
-	  System.out.println(Arrays.deepToString(choleskyDecompose(test2))+" \n");
+/*
+ * Class: CholeskyDecomposition
+ * Description: Carries out and tests Cholesky Decomposition on SPD Matrices
+ */
+public class CholeskyDecomposition 
+{
+	/*
+	 * Name: main()
+	 * Parameters: String[]
+	 * Output : void
+	 * Description: Main function.
+	 */
+	public static void main(String[] args)
+	{
+	  double[][] decomposedMatrix =	choleskyDecompose(TestMatrices.test2);
+      System.out.println(Arrays.deepToString(matrixSolver(TestMatrices.test1, TestMatrices.test2B))+ " \n");
+      System.out.println(Arrays.deepToString(matrixTranspose(TestMatrices.test2))+ " \n");
 	}
 	
-	public static double[][] choleskyDecompose(double[][] inputMatrix){
+	/*
+	 * Name: choleskyDecompose()
+	 * Parameters: double[][]
+	 * Output : double[][]
+	 * Description: Takes a 2D Matrix inputMatrix and does Cholesky Decomposition on it to 
+	 * 				produce a lower triangular matrix output. 
+	 */
+	public static double[][] choleskyDecompose(double[][] inputMatrix)
+	{
 		int lengthOfMatrix = inputMatrix.length;
-		double[][] l_Matrix = new double[lengthOfMatrix][lengthOfMatrix]; //automatically initialzed to 0's
-		for(int i = 0; i< lengthOfMatrix;i++){
-			for(int k = 0; k < (i+1); k++){
+		double[][] outputMatrix = new double[lengthOfMatrix][lengthOfMatrix]; 
+		for(int i = 0; i < lengthOfMatrix;i++)
+		{
+			for(int k = 0; k < (i+1); k++)
+			{
 				double sum = 0;
-				for(int j = 0; j < k; j++){
-					sum += l_Matrix[i][j] * l_Matrix[k][j];
+				for(int j = 0; j < k; j++)
+				{
+					sum += outputMatrix[i][j] * outputMatrix[k][j];
 				}
-				l_Matrix[i][k] = (i == k) ? Math.sqrt(inputMatrix[i][i] - sum) : (1.0 / l_Matrix[k][k] * (inputMatrix[i][k] - sum));
+				if (i==k)
+				{
+					outputMatrix[i][k] = Math.sqrt(inputMatrix[i][i] - sum);
+				}
+				else
+				{
+					outputMatrix[i][k]= (1.0 / outputMatrix[k][k] * (inputMatrix[i][k] - sum));
+				}	
 			}
 		}
-		return l_Matrix;
+		return outputMatrix;
 	}
-	
+	public static double[][] matrixSolver(double[][] A, double[][] B)
+	{
+		if(B.length != A.length)
+		{
+			System.out.println("Matrix row dimensions must agree.");
+		}
+		int aRowDimension = A.length;
+		int bColumnDimension = B[0].length;
+		double[][] X = new double[aRowDimension][bColumnDimension];
+		double[][] decomposedMatrix = choleskyDecompose(A);
+	    for (int k = 0; k < aRowDimension; k++)
+	    {
+	          for (int i = k+1; i < aRowDimension; i++)
+	          {
+	             for (int j = 0; j < bColumnDimension; j++)
+	             {
+	                X[i][j] -= X[k][j]*decomposedMatrix[i][k];
+	             }
+	          }
+	     }
+		
+//		 for (int k = 0; k < aRowDimension; k++) 
+//		 {
+//	         for (int i = k+1; i < aRowDimension; i++) 
+//	         {
+//	            for (int j = 0; j < bColumnDimension; j++) 
+//	            {
+//	               X[i][j] -= X[k][j]*A[i][k];
+//	            }
+//	         }
+//	      } 
+//		 for (int k = aRowDimension-1; k >= 0; k--)
+//		 {
+//	         for (int j = 0; j < bColumnDimension; j++) 
+//	         {
+//	            X[k][j] /= A[k][k];
+//	         }
+//	         for (int i = 0; i < k; i++) 
+//	         {
+//	            for (int j = 0; j < aRowDimension; j++) 
+//	            {
+//	               X[i][j] -= X[k][j]*A[i][k];
+//	            }
+//	         }
+//	      }
+		return X;
+	}
 
+	public static double[][] matrixTranspose(double[][] inputMatrix)
+	{
+		 int m = inputMatrix.length;
+		 int n = inputMatrix[0].length;
+
+		 double[][] trasposedMatrix = new double[n][m];
+
+		    for(int x = 0; x < n; x++)
+		    {
+		        for(int y = 0; y < m; y++)
+		        {
+		            trasposedMatrix[x][y] = inputMatrix[y][x];
+		        }
+		    }
+
+		  return trasposedMatrix;
+	}
 }
---
+++ Assignment-1/CholeskyDecomposition/src/TestMatrices.java
@@ -0,0 +1,13 @@
+
+public class TestMatrices 
+{
+	public static double[][] test1 = {{25, 15, -5},
+					    {15,18,0},
+						{-5,0,11}};
+	public static double[][] test2 = {{18, 22, 54, 42},
+						{22, 70, 86, 62},
+				        {54, 86, 174, 134},
+				        {42, 62, 134, 106}};
+	public static double[][] test2B = {{5},{5},{5}};
+	
+}
