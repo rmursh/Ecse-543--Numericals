ca62bc207357bd5eb6faca0d0b5a21f0755abb7c
Razi Murshed <razi.murshed@mail.mcgill.ca>
2016-12-05 22:59:57 -0500

    q1 e in progress

--- Assignment-3/MatlabCode/Polynomial.m
+++ Assignment-3/MatlabCode/Polynomial.m
@@ -1,7 +1,7 @@
 X = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0];
 Y = [0.0, 14.7, 36.5, 71.7, 121.4, 197.4];
 xx = 0:.0001:1.9;
-P1 = Lagrange(X,Y);
+%P1 = Lagrange(X,Y);
 Yout = polyval(P1,xx);
 figure;
 plot(Lagrange(X,Y,xx),xx);
@@ -12,7 +12,7 @@ plot(cubicHermiteInterpolation(X,Y,xx),xx);
 
 X= [0, 1.3, 1.4, 1.7, 1.8, 1.9];
 Y = [0.0, 540.6, 1062.8, 8687.4, 13924.3, 22650.2];
-P3 = Lagrange(X,Y);
+%P3 = Lagrange(X,Y);
 figure;
 plot(Lagrange(X,Y,xx),xx);
 
